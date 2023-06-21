fcc80dc9228ce30721a7c4a2a1407d647c5b43f6
beserkerbob <bobvanderstaak@Hotmail.com>
2023-06-21 08:26:20 +0200

    added autodiscoveryscript

---
+++ autoDiscoverycurl.sh
@@ -0,0 +1,17 @@
+curl --location 'https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc' --header 'Content-Type: text/xml; charset=utf-8' --data '<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:exm="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:ext="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Header>
+        <a:Action soap:mustUnderstand="1">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action>
+        <a:To soap:mustUnderstand="1">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To>
+        <a:ReplyTo>
+            <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+        </a:ReplyTo>
+    </soap:Header>
+    <soap:Body>
+        <GetFederationInformationRequestMessage xmlns="http://schemas.microsoft.com/exchange/2010/Autodiscover">
+            <Request>
+                <Domain>targetdomain.com</Domain>
+            </Request>
+        </GetFederationInformationRequestMessage>
+    </soap:Body>
+</soap:Envelope>' | grep -o -P '(?<=<Domain>).*(?=</Domain>)'  | sed 's/<Domain>//g' | sed 's/<\/Domain>/,/g'
